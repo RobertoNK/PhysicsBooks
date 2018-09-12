c18a8fd9498122def251421fe655490f66eaf61b
RobertoNK <37525909+RobertoNK@users.noreply.github.com>
2018-09-12 15:20:55 -0400

    to php

--- index2.php
+++ index2.php
@@ -28,14 +28,14 @@ echo <<<_mierda
     <li> <a href="https://www.amazon.com/Physics-Scientists-Engineers-Strategic-Approach/dp/0133942651/ref=sr_1_2?s=books&ie=UTF8&qid=1536045526&sr=1-2&keywords=knight+physics+for+scientists+and+engineers+4th+edition" target="_blank">Modern Physics</a></li>
     <li> <a href="https://www.amazon.com/Introduction-Electrodynamics-January-1-2012/dp/8120347765/ref=sr_1_11?s=books&ie=UTF8&qid=1536046329&sr=1-11&keywords=electricity+and+magnetism" target="_blank">Electricity and Magnetism</a></li>
     <li> <a href="https://www.amazon.com/Advanced-Engineering-Mathematics-Erwin-Kreyszig/dp/0470458364/ref=pd_sbs_14_18?_encoding=UTF8&pd_rd_i=0470458364&pd_rd_r=026e12f2-b015-11e8-97de-971d4c54e3db&pd_rd_w=iUwvR&pd_rd_wg=cJ2gS&pf_rd_i=desktop-dp-sims&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=0bb14103-7f67-4c21-9b0b-31f42dc047e7&pf_rd_r=2A67FC00S7APZ7XVKMVG&pf_rd_s=desktop-dp-sims&pf_rd_t=40701&psc=1&refRID=2A67FC00S7APZ7XVKMVG" target="_blank">Partial differential Equations</a></li>
-    <li><a href="noBook.html" target="_blank">Advanced Laboratoy</a></li>
+    <li><a href="noBook.php" target="_blank">Advanced Laboratoy</a></li>
   </ul>
   <li>Fourth Year</li>
   <ul>
     <li> <a href="https://www.amazon.com/Introduction-Thermal-Physics-Daniel-Schroeder/dp/0201380277/ref=pd_sim_14_11?_encoding=UTF8&pd_rd_i=0201380277&pd_rd_r=38d169a5-b015-11e8-b11d-915b29cfa4b3&pd_rd_w=Gi6MD&pd_rd_wg=vAoob&pf_rd_i=desktop-dp-sims&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=18bb0b78-4200-49b9-ac91-f141d61a1780&pf_rd_r=WR37MZVWFKTY66W4KP73&pf_rd_s=desktop-dp-sims&pf_rd_t=40701&psc=1&refRID=WR37MZVWFKTY66W4KP73" target="_blank">Thermal Physics</a></li>
     <li><a href="https://www.amazon.com/Classical-Mechanics-John-R-Taylor/dp/189138922X/ref=sr_1_2?s=books&ie=UTF8&qid=1536046642&sr=1-2&keywords=classical+dynamics" target="_blank">Classical Mechanics</a></li>
     <li><a href="https://www.amazon.com/Advanced-Engineering-Mathematics-Erwin-Kreyszig/dp/0470458364/ref=pd_sbs_14_18?_encoding=UTF8&pd_rd_i=0470458364&pd_rd_r=026e12f2-b015-11e8-97de-971d4c54e3db&pd_rd_w=iUwvR&pd_rd_wg=cJ2gS&pf_rd_i=desktop-dp-sims&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=0bb14103-7f67-4c21-9b0b-31f42dc047e7&pf_rd_r=2A67FC00S7APZ7XVKMVG&pf_rd_s=desktop-dp-sims&pf_rd_t=40701&psc=1&refRID=2A67FC00S7APZ7XVKMVG" target="_blank">Vector Calculus</a> </li>
-    <li><a href="noBook.html" target="_blank">Research Project</a></li>
+    <li><a href="noBook.php" target="_blank">Research Project</a></li>
   </ul>
 </ul>
   </div>
@@ -45,8 +45,8 @@ echo <<<_mierda
   <ul>
     <li><a href="https://www.amazon.com/Introduction-Quantum-Mechanics-David-Griffiths/dp/1107189632/ref=sr_1_5?s=books&ie=UTF8&qid=1536046741&sr=1-5&keywords=quantum+mechanics" target="_blank">Quantum Mechancis</a></li>
     <li><a href="https://www.amazon.com/Astrophysics-Processes-Physics-Astronomical-Phenomena/dp/1107677246/ref=sr_1_1?s=books&ie=UTF8&qid=1536046840&sr=1-1&keywords=astrophysics+processes" target="_blank">Introduction to Astrophysics</a></li>
-    <li><a href="noBook.html"target="_blank">Computational Physics</a></li>
-    <li><a href="noBook.html" target="_blank">Embeded Systems</a></li>
+    <li><a href="noBook.php"target="_blank">Computational Physics</a></li>
+    <li><a href="noBook.php" target="_blank">Embeded Systems</a></li>
     <li><a href="https://www.amazon.com/Modern-Spectroscopy-J-Michael-Hollas/dp/0470844167/ref=sr_1_3?s=books&ie=UTF8&qid=1536046881&sr=1-3&keywords=spectroscopy" target="_blank">Spectroscopy</a></li>
   </ul>
   <li>Math needed</li>
